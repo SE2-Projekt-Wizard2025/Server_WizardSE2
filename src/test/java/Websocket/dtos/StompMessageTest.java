a50e4334c8eac6d799859fc3461b29be78227453
thomasstu <thomasstu@edu.aau.at>
2025-04-04 16:38:18 +0200

    Add unit tests for Websocket Classes.

---
+++ src/test/java/Websocket/dtos/StompMessageTest.java
@@ -0,0 +1,8 @@
+package Websocket.dtos;
+
+public class StompMessageTest {
+    /**
+     Class exists for completeness, but currently no tests are required, due to it being very simple.
+     If future changes make it more complex, tests will be added here.
+     */
+}
