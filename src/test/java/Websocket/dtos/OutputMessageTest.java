ec95e794ddaccc0886edf552610a264bbf78eed9
thomasstu <thomasstu@edu.aau.at>
2025-04-06 14:21:45 +0200

    Add unit tests for OutputMessage and StompMessage to get > 80% coverage.

--- src/test/java/Websocket/dtos/OutputMessageTest.java
+++ src/test/java/Websocket/dtos/OutputMessageTest.java
@@ -1,8 +1,25 @@
 package Websocket.dtos;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class OutputMessageTest {
-    /**
-    Class exists for completeness, but currently no tests are required, due to it being very simple.
-     If future changes make it more complex, tests will be added here.
-     */
+
+    @Test
+    void testGetFrom() {
+        OutputMessage message = new OutputMessage("Alice", "Hello!", "2025-04-04T12:00:00");
+        assertEquals("Alice", message.getFrom());
+    }
+
+    @Test
+    void testGetText() {
+        OutputMessage message = new OutputMessage("Alice", "Hello!", "2025-04-04T12:00:00");
+        assertEquals("Hello!", message.getText());
+    }
+
+    @Test
+    void testGetTime() {
+        OutputMessage message = new OutputMessage("Alice", "Hello!", "2025-04-04T12:00:00");
+        assertEquals("2025-04-04T12:00:00", message.getTime());
+    }
 }
