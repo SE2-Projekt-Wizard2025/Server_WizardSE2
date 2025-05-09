9f7fa2581c2e70c6d942a80db04184b91cc5ab9f
thomasstu <thomasstu@edu.aau.at>
2025-05-09 20:39:59 +0200

    fix: hashCode() für Karte + Test angepasst

--- src/main/java/com/aau/wizard/model/Card.java
+++ src/main/java/com/aau/wizard/model/Card.java
@@ -1,54 +1,9 @@
-
-
-//Des is dei alter Code Elias ************************************************************************************************************
-
-/*package com.aau.wizard.model;
-
-import com.aau.wizard.model.enums.CardColor;
-import com.aau.wizard.model.enums.CardType;
-import com.aau.wizard.model.enums.CardValue;
-
-public class Card {
-    private CardColor color;
-    private CardValue value;
-    private CardType type;
-
-    public Card(CardColor color, CardValue value, CardType type) {
-        this.color = color;
-        this.value = value;
-        this.type = type;
-    }
-
-    public CardColor getColor() {
-        return color;
-    }
-
-    public void setColor(CardColor color) {
-        this.color = color;
-    }
-
-    public CardValue getValue() {
-        return value;
-    }
-
-    public void setValue(CardValue value) {
-        this.value = value;
-    }
-
-    public CardType getType() {
-        return type;
-    }
-
-    public void setType(CardType type) {
-        this.type = type;
-    }
-}
-*/
-
 package com.aau.wizard.model;
 
 import com.aau.wizard.model.enums.CardType;
 import com.aau.wizard.model.enums.CardSuit;
+
+import java.util.Objects;
 
 public class Card {
     private final CardSuit cardSuit;
@@ -89,5 +44,20 @@ public class Card {
             default:
                 return value + " of " + cardSuit.name().toLowerCase();
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Card)) return false;
+        Card card = (Card) o;
+        return value == card.value &&
+                cardSuit == card.cardSuit &&
+                type == card.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cardSuit, value, type);
     }
 }
--- src/test/java/model/CardTest.java
+++ src/test/java/model/CardTest.java
@@ -1,53 +1,100 @@
 package model;
 
+import com.aau.wizard.model.enums.CardSuit;
 import com.aau.wizard.model.Card;
 import com.aau.wizard.model.enums.CardType;
-
-import static com.aau.wizard.testutil.TestDataFactory.*;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardTest {
 
-    private Card testCard;
-
-    @BeforeEach
-    void setup() {
-        testCard = createDefaultCard();
-    }
-
     @Test
-    void testConstructorAndGetters() {
-        assertCardValues(testCard);
-    }
+    void testNumberCardCreation() {
+        Card card = new Card(CardSuit.RED, 7);
 
-    @Test
-    void testSetColor() {
-        testCard.setColor(CardColor.GREEN);
-
-        assertEquals(CardColor.GREEN, testCard.getColor());
-    }
-
-    @Test
-    void testSetValue() {
-        testCard.setValue(CardValue.EIGHT);
-
-        assertEquals(CardValue.EIGHT, testCard.getValue());
-    }
-
-    @Test
-    void testSetType() {
-        testCard.setType(CardType.WIZARD);
-
-        assertEquals(CardType.WIZARD, testCard.getType());
-    }
-
-    private void assertCardValues(Card card) {
-        assertEquals(CardColor.RED, card.getColor());
-        assertEquals(CardValue.ONE, card.getValue());
+        assertEquals(CardSuit.RED, card.getSuit());
+        assertEquals(7, card.getValue());
         assertEquals(CardType.NUMBER, card.getType());
+        assertEquals("7 of red", card.toString());
+    }
+
+    @Test
+    void testJesterCardCreation() {
+        Card card = new Card(CardSuit.YELLOW, 0);
+
+        assertEquals(CardSuit.YELLOW, card.getSuit());
+        assertEquals(0, card.getValue());
+        assertEquals(CardType.JESTER, card.getType());
+        assertEquals("Jester (yellow)", card.toString());
+    }
+
+    @Test
+    void testWizardCardCreation() {
+        Card card = new Card(CardSuit.BLUE, 14);
+
+        assertEquals(CardSuit.BLUE, card.getSuit());
+        assertEquals(14, card.getValue());
+        assertEquals(CardType.WIZARD, card.getType());
+        assertEquals("Wizard (blue)", card.toString());
+    }
+
+    @Test
+    void testEdgeCaseNumberCards() {
+        Card lowestNumber = new Card(CardSuit.GREEN, 1);
+        Card highestNumber = new Card(CardSuit.GREEN, 13);
+
+        assertEquals(CardType.NUMBER, lowestNumber.getType());
+        assertEquals(CardType.NUMBER, highestNumber.getType());
+        assertEquals("1 of green", lowestNumber.toString());
+        assertEquals("13 of green", highestNumber.toString());
+    }
+
+    @Test
+    void testAllSuitsForNumberCard() {
+        for (CardSuit suit : CardSuit.values()) {
+            Card card = new Card(suit, 5);
+            assertEquals(suit, card.getSuit());
+            assertTrue(card.toString().contains(suit.name().toLowerCase()));
+        }
+    }
+
+    @Test
+    void testAllSuitsForJester() {
+        for (CardSuit suit : CardSuit.values()) {
+            Card card = new Card(suit, 0);
+            assertEquals(suit, card.getSuit());
+            assertTrue(card.toString().contains("Jester"));
+            assertTrue(card.toString().contains(suit.name().toLowerCase()));
+        }
+    }
+
+    @Test
+    void testAllSuitsForWizard() {
+        for (CardSuit suit : CardSuit.values()) {
+            Card card = new Card(suit, 14);
+            assertEquals(suit, card.getSuit());
+            assertTrue(card.toString().contains("Wizard"));
+            assertTrue(card.toString().contains(suit.name().toLowerCase()));
+        }
+    }
+
+    @Test
+    void testSpecialSuitCards() {
+        Card numberSpecial = new Card(CardSuit.SPECIAL, 5);
+        Card jesterSpecial = new Card(CardSuit.SPECIAL, 0);
+        Card wizardSpecial = new Card(CardSuit.SPECIAL, 14);
+
+        assertEquals("5 of special", numberSpecial.toString());
+        assertEquals("Jester (special)", jesterSpecial.toString());
+        assertEquals("Wizard (special)", wizardSpecial.toString());
+    }
+
+    @Test
+    void hashCode_shouldBeEqualForSameCard() {
+        Card card1 = new Card(CardSuit.RED, 5);
+        Card card2 = new Card(CardSuit.RED, 5);
+
+        assertEquals(card1.hashCode(), card2.hashCode(),
+                "Two cards with same suit and value should have same hash code");
     }
 }
