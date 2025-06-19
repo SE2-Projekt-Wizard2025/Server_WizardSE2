a4c3f0027eb5432850c753679fb5c151130f46e4
thomasstu <thomasstu@edu.aau.at>
2025-06-19 21:31:33 +0200

    test: Tests für JesterCard hinzugefügt

---
+++ src/test/java/model/JesterCardTest.java
@@ -0,0 +1,55 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.aau.wizard.model.JesterCard;
+import com.aau.wizard.model.WizardCard;
+import org.junit.jupiter.api.Test;
+import com.aau.wizard.model.enums.CardSuit;
+
+public class JesterCardTest {
+
+    @Test
+    void equals_ShouldBeReflexive() {
+        JesterCard card = new JesterCard(CardSuit.SPECIAL);
+        assertTrue(card.equals(card));
+
+    }
+
+    @Test
+    void equals_WithNull_ShouldReturnFalse() {
+        JesterCard card = new JesterCard(CardSuit.SPECIAL);
+        assertFalse(card.equals(null));
+    }
+
+    @Test
+    void equals_WithDifferentClass_ShouldReturnFalse() {
+        JesterCard card = new JesterCard(CardSuit.SPECIAL);
+        WizardCard wizard = new WizardCard(CardSuit.SPECIAL);
+        assertFalse(card.equals(wizard));
+    }
+
+    @Test
+    void equals_WithSameClass_ShouldReturnTrue() {
+        JesterCard card1 = new JesterCard(CardSuit.SPECIAL);
+        JesterCard card2 = new JesterCard(CardSuit.SPECIAL);
+        assertTrue(card1.equals(card2));
+    }
+
+
+    @Test
+    void hashCode_ForEqualObjects_ShouldBeEqual() {
+        JesterCard card1 = new JesterCard(CardSuit.SPECIAL);
+        JesterCard card2 = new JesterCard(CardSuit.SPECIAL);
+        assertEquals(card1.hashCode(), card2.hashCode());
+    }
+
+
+    @Test
+    void hashCode_ForSameObject_ShouldBeConsistent() {
+        JesterCard card = new JesterCard(CardSuit.SPECIAL);
+        int initialHash = card.hashCode();
+        assertEquals(initialHash, card.hashCode());
+    }
+}
+
