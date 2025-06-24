95f4f5a5856385063812fae51575ea452750711d
thomasstu <thomasstu@edu.aau.at>
2025-06-24 14:39:26 +0200

    refactor: public von Klasse wurde entfernt

--- src/test/java/model/JesterCardTest.java
+++ src/test/java/model/JesterCardTest.java
@@ -7,7 +7,7 @@ import com.aau.wizard.model.WizardCard;
 import org.junit.jupiter.api.Test;
 import com.aau.wizard.model.enums.CardSuit;
 
-public class JesterCardTest {
+class JesterCardTest {
 
     @Test
     void equals_ShouldBeReflexive() {
