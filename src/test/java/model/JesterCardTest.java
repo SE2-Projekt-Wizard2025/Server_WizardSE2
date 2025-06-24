f9c3eaddf672b1d7a521f103562339433bbb7261
thomasstu <thomasstu@edu.aau.at>
2025-06-24 14:41:14 +0200

    refactor: asserts ausgebessert

--- src/test/java/model/JesterCardTest.java
+++ src/test/java/model/JesterCardTest.java
@@ -12,28 +12,28 @@ class JesterCardTest {
     @Test
     void equals_ShouldBeReflexive() {
         JesterCard card = new JesterCard(CardSuit.SPECIAL);
-        assertTrue(card.equals(card));
+        assertEquals(card, card);
 
     }
 
     @Test
     void equals_WithNull_ShouldReturnFalse() {
         JesterCard card = new JesterCard(CardSuit.SPECIAL);
-        assertFalse(card.equals(null));
+        assertNotEquals(null, card);
     }
 
     @Test
     void equals_WithDifferentClass_ShouldReturnFalse() {
         JesterCard card = new JesterCard(CardSuit.SPECIAL);
         WizardCard wizard = new WizardCard(CardSuit.SPECIAL);
-        assertFalse(card.equals(wizard));
+        assertNotEquals(card, wizard);
     }
 
     @Test
     void equals_WithSameClass_ShouldReturnTrue() {
         JesterCard card1 = new JesterCard(CardSuit.SPECIAL);
         JesterCard card2 = new JesterCard(CardSuit.SPECIAL);
-        assertTrue(card1.equals(card2));
+        assertEquals(card1, card2);
     }
 
 
